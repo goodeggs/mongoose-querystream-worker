137eae754f34010bac6b1ebe7eaeac9b4ab3aee2
Adam Hull <adam@hmlad.com>
2013-08-20 17:14:31 -0700

    Declare variables

--- mongoose-querystream-worker.js
+++ mongoose-querystream-worker.js
@@ -1,7 +1,6 @@
-QueryStream = require('mongoose/lib/querystream');
-streamWorker = require('stream-worker');
-
-DEFAULT_CONCURRENCY_LIMIT = 10;
+var QueryStream = require('mongoose/lib/querystream'),
+    streamWorker = require('stream-worker'),
+    DEFAULT_CONCURRENCY_LIMIT = 10;
 
 QueryStream.prototype.concurrency = function (max) {
   this._concurrency = max;
@@ -12,4 +11,4 @@ QueryStream.prototype.work = function (worker, done) {
   var concurrency = this._concurrency || DEFAULT_CONCURRENCY_LIMIT;
   streamWorker(this, concurrency, worker, done);
   return this;
-}
+};
